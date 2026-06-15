f80ba24ba7f93f45bcc94e0bdc90cbaf97672e2b
martapolii <m.polishchuk@hotmail.ca>
2026-06-15 13:30:21 -0400

    Created new project & added main files needed for MVVM

---
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/CareerItem.kt
@@ -0,0 +1,12 @@
+package com.example.martapolishchuk_comp304lab2_ex1
+
+// Career Item class - this will be the data/model class which stores the schema for each type of career item
+
+data class CareerItem(
+    // data members
+    val title: String,
+    val category: String,
+    val progressStatus: String,
+    val completionIndicator: String
+
+)
