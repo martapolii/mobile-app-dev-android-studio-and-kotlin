994e216164d4e5d236161132be7a2d157c557159
martapolii <m.polishchuk@hotmail.ca>
2026-06-15 16:28:09 -0400

    Added data members to Career Item Model class

--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/CareerItem.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/CareerItem.kt
@@ -3,10 +3,9 @@ package com.example.martapolishchuk_comp304lab2_ex1
 // Career Item class - this will be the data/model class which stores the schema for each type of career item
 
 data class CareerItem(
-    // data members
+    // data members - need: title, category, progress status, completion indicator
     val title: String,
-    val category: String,
+    val category: String, //["Skill Development", "Certification", "Academic Project", "Internship Application", "Career Goal"]
     val progressStatus: String,
     val completionIndicator: String
-
 )
