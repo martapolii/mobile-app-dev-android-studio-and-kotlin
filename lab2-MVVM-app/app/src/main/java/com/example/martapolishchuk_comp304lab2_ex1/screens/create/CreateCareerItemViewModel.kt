b5a58186f9fc47cca45da1c4749696cd5a69a854
martapolii <m.polishchuk@hotmail.ca>
2026-06-18 22:50:13 -0400

    moved to create folder

--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/screens/create/CreateCareerItemViewModel.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/screens/create/CreateCareerItemViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.martapolishchuk_comp304lab2_ex1.screens.create
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.martapolishchuk_comp304lab2_ex1.data.CareerItem
+import com.example.martapolishchuk_comp304lab2_ex1.data.CareerItemRepository
+
+class CreateCareerItemViewModel (private val repository: CareerItemRepository) : ViewModel() {
+
+    // LiveData to observe changes in the list of Career Items
+    val careerItems: LiveData<List<CareerItem>> = repository.getCareerItems()
+
+   // Business Logic:
+
+    // 1. Function to add new Career Items
+    fun addCareerItem(title: String, category: String, progressStatus: String, completionIndicator: Boolean){
+        val newCareerItem = CareerItem(title, category, progressStatus, completionIndicator)
+        repository.addCareerItem(newCareerItem)
+    }
+}
