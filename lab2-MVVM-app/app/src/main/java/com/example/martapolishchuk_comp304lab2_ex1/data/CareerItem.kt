b82b45706718d4c17327fe651516a7cdfbc2fc1b
martapolii <m.polishchuk@hotmail.ca>
2026-06-18 22:47:42 -0400

    added sample data to data class

--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/data/CareerItem.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/data/CareerItem.kt
@@ -1,2 +1,21 @@
-package com.example.martapolishchuk_comp304lab2_ex1.data 
+package com.example.martapolishchuk_comp304lab2_ex1.data
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+
+data class CareerItem(
+    // data members - need: title, category, progress status, completion indicator
+    val title: String,
+    val category: String, //["Skill Development", "Certification", "Academic Project", "Internship Application", "Career Goal"]
+    val progressStatus: String,
+    val completionIndicator: Boolean
+
+
+)
+// sample data
+var careerItemList = mutableStateListOf(
+    CareerItem("COMP304 Lab", "Centennial College", "2026-06-09", true),
+    CareerItem("COMP304 Lab", "Centennial College", "2026-06-09", false),
+    CareerItem("COMP304 Lab", "Centennial College", "2026-06-09", false)
+)
 
