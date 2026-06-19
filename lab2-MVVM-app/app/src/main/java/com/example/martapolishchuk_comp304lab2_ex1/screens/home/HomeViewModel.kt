3e0a3c898111f1f6cb36c89e93871b2b55fec95e
martapolii <m.polishchuk@hotmail.ca>
2026-06-18 22:49:20 -0400

    moved code to home screen

--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/screens/home/HomeViewModel.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/screens/home/HomeViewModel.kt
@@ -1,21 +1,6 @@
 package com.example.martapolishchuk_comp304lab2_ex1.screens.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import com.example.martapolishchuk_comp304lab2_ex1.data.CareerItem
-import com.example.martapolishchuk_comp304lab2_ex1.data.CareerItemRepository
+class HomeViewModel {
 
-// Career Item View Model -
-
-class CareerItemViewModel(private val repository: CareerItemRepository) : ViewModel() {
-
-    val careerItems: LiveData<List<CareerItem>> = repository.getCareerItems()
-
-// Need to implement logic for the functions in the repository here:
-
-    // 1. Function to add new Career Items
-    fun addCareerItem(title: String, category: String, progressStatus: String, completionIndicator: String){
-        val newCareerItem = CareerItem(title, category, progressStatus, completionIndicator)
-        repository.addCareerItem(newCareerItem)
-    }
 }
+
