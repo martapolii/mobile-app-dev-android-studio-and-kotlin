4bfefa68380084a20454b9553a71e35b484627e0
martapolii <m.polishchuk@hotmail.ca>
2026-06-18 22:48:20 -0400

    moved data files to own folder

--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/data/CareerItemRepository.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/data/CareerItemRepository.kt
@@ -1,2 +1,17 @@
-package com.example.martapolishchuk_comp304lab2_ex1.data 
+package com.example.martapolishchuk_comp304lab2_ex1.data
 
+import androidx.lifecycle.LiveData
+
+// Career Item Repository - interface for repository to implement dependency injection
+
+interface CareerItemRepository {
+    // signature's of methods to be used to manipulate the data model are listed here:
+
+    // 1. Get a Career Item
+    fun getCareerItems(): LiveData<List<CareerItem>> // get details of the list
+
+    // 2. Add a new Career Item
+    fun addCareerItem(careerItem: CareerItem) // pass the object
+
+    // 3. Edit an existing Career Item
+}
--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/data/CareerItemRepositoryImpl.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/data/CareerItemRepositoryImpl.kt
@@ -1,2 +1,19 @@
-package com.example.martapolishchuk_comp304lab2_ex1.data 
+package com.example.martapolishchuk_comp304lab2_ex1.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+// Career Item Repository Implementation
+class CareerItemRepositoryImpl : CareerItemRepository { // inherits from the repository
+
+    // live data to hold the list of career items:
+    private val careerItemList = MutableLiveData<List<CareerItem>>(emptyList())
+
+    // definition of the getCareerItems method:
+    override fun getCareerItems(): LiveData<List<CareerItem>> = careerItemList
+
+    // definition of the addCareerItem method:
+    override fun addCareerItem(careerItem: CareerItem) {
+        careerItemList.value = careerItemList.value?.plus(element = careerItem)
+    }
+}
