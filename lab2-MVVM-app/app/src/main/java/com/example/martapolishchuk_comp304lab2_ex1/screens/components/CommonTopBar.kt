f719ce7741098247b72ed3482e93aa715bdf04df
martapolii <m.polishchuk@hotmail.ca>
2026-06-18 22:50:38 -0400

    created a reusable top bar composable

--- lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/screens/components/CommonTopBar.kt
+++ lab2-MVVM-app/app/src/main/java/com/example/martapolishchuk_comp304lab2_ex1/screens/components/CommonTopBar.kt
@@ -1,2 +1,34 @@
 package com.example.martapolishchuk_comp304lab2_ex1.screens.components
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+// Created a reusable composable function for the Top App Bar to use on all screens - will contain the app title
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonTopBar(
+    title: String,
+    onBackClick: (() -> Unit)? = null // optional back button -> only appears if 'onBackClick' action is defined in the activity
+){
+    CenterAlignedTopAppBar(
+        title = { Text(text = title) },
+        // back button
+        navigationIcon = {
+            if (onBackClick != null) {
+                IconButton(onClick = onBackClick) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                } //end of icon button
+            }
+        } // end of navigation icon
+    ) // end of top bar
+}
