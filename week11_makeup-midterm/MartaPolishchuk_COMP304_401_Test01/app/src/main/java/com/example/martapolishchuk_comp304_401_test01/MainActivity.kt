bb2363ea1d33bd51a86d6675307ee35618027506
martapolii <m.polishchuk@hotmail.ca>
2026-07-28 18:52:31 -0400

    make up midterm - opened new project

---
+++ week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.martapolishchuk_comp304_401_test01
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.martapolishchuk_comp304_401_test01.ui.theme.MartaPolishchuk_COMP304_401_Test01Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MartaPolishchuk_COMP304_401_Test01Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MartaPolishchuk_COMP304_401_Test01Theme {
+        Greeting("Android")
+    }
+}
