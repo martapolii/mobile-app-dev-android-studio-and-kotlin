702149b1e9993e3461c94475ccb083905d98f9c8
martapolii <m.polishchuk@hotmail.ca>
2026-07-28 19:09:50 -0400

    contact data class logic

--- week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/data/Contact.kt
+++ week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/data/Contact.kt
@@ -1,17 +1,40 @@
 package com.example.martapolishchuk_comp304_401_test01.data
 
-
-
 // contact data class
-// text fields for ID (3 digits)
-// name
-// cell phone (10 digits)
-// email
-// contact type (drop down): Family, Personal, Relative
-// favourite (radio button): Yes, No
-
-class Contact {
 
 
+data class Contact {
 
+    // text fields for ID (3 digits)
+    val contactId: String,
+
+    // name
+    val name: String,
+
+    // cell phone (10 digits)
+    val cellPhone: String,
+
+    // email
+    val email: String,
+
+    // contact type
+    val contactType: String,
+
+    // favourite
+    val favourite: String
 }
+
+// contact type (drop down): Family, Personal, Relative
+val contactTypeOptions = listOf(
+    "Family",
+    "Personal",
+    "Relative"
+)
+
+// favourite (radio button): Yes, No
+val favouriteOptions = listOf(
+    "Yes",
+    "No"
+)
+
+// validate input
