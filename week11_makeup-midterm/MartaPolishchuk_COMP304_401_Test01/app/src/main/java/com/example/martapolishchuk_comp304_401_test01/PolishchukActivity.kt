d25a7366acc011b396381d5b46d4e14327518646
martapolii <m.polishchuk@hotmail.ca>
2026-07-28 19:43:57 -0400

    added drop down menu logic for contact type

--- week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/PolishchukActivity.kt
+++ week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/PolishchukActivity.kt
@@ -5,11 +5,18 @@
 package com.example.martapolishchuk_comp304_401_test01
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -21,6 +28,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.martapolishchuk_comp304_401_test01.data.Contact
+import com.example.martapolishchuk_comp304_401_test01.data.contactTypeOptions
+import kotlin.math.exp
 
 @Composable
 fun AddContactScreen(
@@ -30,7 +39,7 @@ fun AddContactScreen(
     var name by remember {mutableStateOf("") }
     var cellPhone by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
-    var contactType by remember { mutableStateOf("") }
+    var contactType by remember { mutableStateOf(contactTypeOptions[0]) } // default will be family (bc index 0 in list - see list in data class)
 
     Column(
         modifier = Modifier
@@ -46,18 +55,41 @@ fun AddContactScreen(
         // field: contact ID
         OutlinedTextField(
             value = contactId,
-            onValueChange = {name = it},
+            onValueChange = {contactId = it},
             label = {Text("Contact ID")},
             modifier = Modifier.fillMaxWidth()
         )
 
         // field: name
+        OutlinedTextField(
+            value = name,
+            onValueChange = {name = it},
+            label = {Text("Name")},
+            modifier = Modifier.fillMaxWidth()
+        )
 
         // field: cellPhone
+        OutlinedTextField(
+            value = cellPhone,
+            onValueChange = {cellPhone = it},
+            label = {Text("Cell Phone")},
+            modifier = Modifier.fillMaxWidth()
+        )
 
         // field: email
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = {Text("Email")},
+            modifier = Modifier.fillMaxWidth()
+        )
 
         // drop-down: contact type
+        MinimalDropdownMenu(
+            selectedContactType = contactType,
+            onContactTypeSelected = { contactType = it }
+        )
+
 
         // radio button: favourite
 
@@ -68,12 +100,44 @@ fun AddContactScreen(
 
 
     } // column
-    
+
 
 
 
 
 } // end of addContactScreen
+
+// drop-down: contact type (Family, Personal, Relative) (https://developer.android.com/develop/ui/compose/components/menu?authuser=1)
+@Composable
+fun MinimalDropdownMenu(
+    selectedContactType: String,
+    onContactTypeSelected: (String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+        Box(
+            modifier = Modifier.padding(16.dp)
+        ){
+            Button(onClick = {expanded = true}) {
+                Text(selectedContactType)
+            }//button
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = {expanded = false}
+            ) {
+                contactTypeOptions.forEach{ option ->
+                    DropdownMenuItem(
+                        text = {Text(option)},
+                        onClick = {
+                            onContactTypeSelected(option)
+                            expanded = false
+                        }
+                    )
+            }//dropdownmenuitem
+        }//dropdownmenu
+    }//box
+}
 
 
 
