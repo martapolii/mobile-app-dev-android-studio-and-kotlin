b7c9a55d66880e357fde83c4e3a6e11b5c722261
martapolii <m.polishchuk@hotmail.ca>
2026-07-28 19:16:54 -0400

    creating add contact page/form

--- week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/PolishchukActivity.kt
+++ week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/PolishchukActivity.kt
@@ -4,3 +4,36 @@
 
 package com.example.martapolishchuk_comp304_401_test01
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.martapolishchuk_comp304_401_test01.data.Contact
+
+@Composable
+fun AddContactScreen(
+    onSave:(Contact)->Unit
+){
+    var contactId by remember{mutableStateOf("")}
+    var name by remember {mutableStateOf("") }
+    var cellPhone by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var contactType by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) { }
+}
+
+
+
