05f931580cc59d8cbec3ddb266eeec853d305ef5
martapolii <m.polishchuk@hotmail.ca>
2026-07-28 19:10:03 -0400

    contact view model rough logic

--- week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/data/ContactViewModel.kt
+++ week11_makeup-midterm/MartaPolishchuk_COMP304_401_Test01/app/src/main/java/com/example/martapolishchuk_comp304_401_test01/data/ContactViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.martapolishchuk_comp304_401_test01.data
 
-class ContactViewModel {
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel: ViewModel() {
+    private val_contacts = mutableStateListOf<Contact>()
+    val contacts:List<Contact>= _contacts
+
+    fun addContact(contact:Contact) {
+        _contacts.add(contact)
+    }
 }
