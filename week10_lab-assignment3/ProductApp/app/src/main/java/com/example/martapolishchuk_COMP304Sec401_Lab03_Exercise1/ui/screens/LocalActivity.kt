05a275e11c28e5f6d5e69b77004c6d26778dc242
martapolii <m.polishchuk@hotmail.ca>
2026-07-30 20:44:59 -0400

    misc

--- week10_lab-assignment3/ProductApp/app/src/main/java/com/example/martapolishchuk_COMP304Sec401_Lab03_Exercise1/ui/screens/LocalActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.productapp.ui.screens
-
-// LocalActivity.kt
-import android.app.Activity
-import androidx.compose.runtime.compositionLocalOf
-
-val LocalActivity = compositionLocalOf<Activity> {
-    error("LocalActivity not provided")
-}
