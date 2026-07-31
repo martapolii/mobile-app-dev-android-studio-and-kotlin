d0734ad576c6ac65d6d46d1a35a23bebc0ef8305
martapolii <m.polishchuk@hotmail.ca>
2026-07-31 00:16:57 -0400

    movie data class added (one of the first things i did, forgot to commit)

--- week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/src/main/java/com/example/martapolishchuk_comp304_401_lab03_exercise02/data/Movie.kt
+++ week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/src/main/java/com/example/martapolishchuk_comp304_401_lab03_exercise02/data/Movie.kt
@@ -1,12 +1,23 @@
 package com.example.martapolishchuk_comp304_401_lab03_exercise02.data
 
-internal class Movie 
+import androidx.room.PrimaryKey
+import androidx.room.Entity
 
+// Marta Polishchuk - 301432299
+// Assignment 3: Exercise 2
 
-
-
-
-
+// Represents a movie entity in the Room database
+@Entity(tableName = "movies")
+data class Movie(
+    @PrimaryKey val id: Int, // every table must have a primary key
+    val title: String,
+    val director: String,
+    val price: Double,
+    val releaseDate: String,
+    val duration: Int,
+    val genre: String,
+    val isFavorite: Boolean
+)
 
 
 
