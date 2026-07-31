31eee43c992ae5534eac8eb3d75bcd9b973f8883
martapolii <m.polishchuk@hotmail.ca>
2026-07-31 00:16:25 -0400

    updated sdk to 37 bc was causing issues

--- week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/build.gradle.kts
+++ week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.example.martapolishchuk_comp304_401_lab03_exercise02"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -43,11 +43,19 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material3.window.size.class1)
+    implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.room.common.jvm)
+    implementation(libs.androidx.room3.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
