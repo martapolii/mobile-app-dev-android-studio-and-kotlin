0f86df118e20c6f89cd10d9c3389118154b1dbdf
martapolii <m.polishchuk@hotmail.ca>
2026-07-30 23:52:47 -0400

    added this file idk

--- week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/src/main/java/com/example/martapolishchuk_comp304_401_lab03_exercise02/ui/screens/LocalActivity.kt
+++ week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/src/main/java/com/example/martapolishchuk_comp304_401_lab03_exercise02/ui/screens/LocalActivity.kt
@@ -1,4 +1,11 @@
 package com.example.martapolishchuk_comp304_401_lab03_exercise02.ui.screens
 
-class LocalActivity {
+// Marta Polishchuk - 301432299
+// Assignment 3: Exercise 2
+
+import android.app.Activity
+import androidx.compose.runtime.compositionLocalOf
+
+val LocalActivity = compositionLocalOf<Activity> {
+    error("LocalActivity not provided")
 }
