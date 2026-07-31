df91cc71828b5c17c06f08d3d3d1d361cc90c825
martapolii <m.polishchuk@hotmail.ca>
2026-07-30 21:05:25 -0400

    created movie dao

--- week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/src/main/java/com/example/martapolishchuk_comp304_401_lab03_exercise02/data/MovieDao.kt
+++ week10_lab-assignment3/martapolishchuk_COMP304Sec401_Lab03_Exercise02/app/src/main/java/com/example/martapolishchuk_comp304_401_lab03_exercise02/data/MovieDao.kt
@@ -1,4 +1,34 @@
 package com.example.martapolishchuk_comp304_401_lab03_exercise02.data
 
+// Marta Polishchuk - 301432299
+// Assignment 3: Exercise 2
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+// Data Access Object for product-related database operations
+@Dao
 interface MovieDao {
+    @Query("SELECT * FROM movies")
+    fun getAll(): Flow<List<Movie>>    // Retrieves all movies as a Flow
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(movie: Movie) // Inserts a movie, replacing if conflict occurs
+
+    @Update
+    suspend fun update(movie: Movie) // Updates an existing movie
+
+    @Delete
+    suspend fun delete(movie: Movie) // Deletes a product
+
+    @Query("SELECT * FROM movies WHERE isFavorite = 1")
+    fun getFavorites(): Flow<List<Movie>> // Retrieves favorite movies as a Flow
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertSampleProducts(products: List<Movie>) // Inserts sample data, ignoring conflicts
 }
